d651777403d60358edfb70239f7e979d1c9f7194
divine <divineobi07@gmail.com>
2022-05-17 23:25:30 +0100

    add illustration

--- src/components/MainContent/Illustration.js
+++ src/components/MainContent/Illustration.js
@@ -1,13 +1,21 @@
 import Mobile_Image from "../../assets/image-hero-mobile.png";
 import Desktop_Image from "../../assets/image-hero-desktop.png";
+import style from './Illustration.module.css'
 
 const Illustration = () => {
+    console.log(style);
   return (
-    <picture>
-      <source media="(max-width: 500px)" srcSet={Mobile_Image} />
-      <source media="(min-width: 501px)" srcSet={Desktop_Image} />
-      <img src={Mobile_Image} alt="A boy with a Laptop" />
-    </picture>
+    <figure className={style.imageContainer}>
+      <picture>
+        <source media="(max-width: 500px)" srcSet={Mobile_Image} />
+        <source media="(min-width: 501px)" srcSet={Desktop_Image} />
+        <img
+          className={style.image}
+          src={Mobile_Image}
+          alt="A boy with a Laptop"
+        />
+      </picture>
+    </figure>
   );
 };
 
