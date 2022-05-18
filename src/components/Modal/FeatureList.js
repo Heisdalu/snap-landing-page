c89baac7155709afc6dbf6607834a11eb862413d
divine <divineobi07@gmail.com>
2022-05-18 20:38:16 +0100

    update FeaturedList component

--- src/components/Modal/FeatureList.js
+++ src/components/Modal/FeatureList.js
@@ -1,3 +1,6 @@
+import ArrowUp from "../../assets/icon-arrow-up.svg";
+import ArrowDown from "../../assets/icon-arrow-down.svg";
+
 import TodoImg from "../../assets/icon-todo.svg";
 import CalendarImg from "../../assets/icon-calendar.svg";
 import RemainderImg from "../../assets/icon-reminders.svg";
@@ -7,34 +10,45 @@ import style from "./FeatureList.module.css";
 
 const FeatureList = () => {
   return (
-    <ul className={style.innerList}>
-
-
-      <li>
-        <span>
-          <img src={TodoImg} alt="todo list icon" />
+    <li className={style.toggleList}>
+      <section className={style.inner}>
+        <span>Features</span>
+        <span className={style.navigateArrow}>
+          <span>
+            <img src={ArrowUp} alt="arrow up icon" />
+          </span>
+          <span>
+            <img src={ArrowDown} alt="arrow down icon" />
+          </span>
         </span>
-        <span>TodoList</span>
-      </li>
-      <li>
-        <span>
-          <img src={CalendarImg} alt="todo list icon" />
-        </span>
-        <span>Calendar</span>
-      </li>
-      <li>
-        <span>
-          <img src={RemainderImg} alt="todo list icon" />
-        </span>
-        <span>Reminders</span>
-      </li>
-      <li>
-        <span>
-          <img src={PlanImg} alt="todo list icon" />
-        </span>
-        <span>Planning</span>
-      </li>
-    </ul>
+      </section>
+      <ul className={style.innerList}>
+        <li>
+          <span>
+            <img src={TodoImg} alt="todo list icon" />
+          </span>
+          <span>TodoList</span>
+        </li>
+        <li>
+          <span>
+            <img src={CalendarImg} alt="todo list icon" />
+          </span>
+          <span>Calendar</span>
+        </li>
+        <li>
+          <span>
+            <img src={RemainderImg} alt="todo list icon" />
+          </span>
+          <span>Reminders</span>
+        </li>
+        <li>
+          <span>
+            <img src={PlanImg} alt="todo list icon" />
+          </span>
+          <span>Planning</span>
+        </li>
+      </ul>
+    </li>
   );
 };
 
