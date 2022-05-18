685ff573e51042641ff615e496aad58937b5fef9
divine <divineobi07@gmail.com>
2022-05-18 20:36:50 +0100

    update companyList component

--- src/components/Modal/CompanyList.js
+++ src/components/Modal/CompanyList.js
@@ -1,12 +1,28 @@
+import ArrowUp from "../../assets/icon-arrow-up.svg";
+import ArrowDown from "../../assets/icon-arrow-down.svg";
+
 import style from "./CompanyList.module.css";
 
 const CompanyList = () => {
   return (
-    <ul className={style.innerList}>
-      <li>History</li>
-      <li>Our Team</li>
-      <li>Blog</li>
-    </ul>
+    <li className={style.toggleList}>
+      <section className={style.inner}>
+        <span>Company</span>
+        <span className={style.navigateArrow}>
+          <span>
+            <img src={ArrowUp} alt="arrow up icon" />
+          </span>
+          <span>
+            <img src={ArrowDown} alt="arrow down icon" />
+          </span>
+        </span>
+      </section>
+      <ul className={style.innerList}>
+        <li>History</li>
+        <li>Our Team</li>
+        <li>Blog</li>
+      </ul>
+    </li>
   );
 };
 
