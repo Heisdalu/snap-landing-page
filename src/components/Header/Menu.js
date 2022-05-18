721fb4a351c1ff655e5369a1b4c29b7d0b56a084
divine <divineobi07@gmail.com>
2022-05-18 11:57:14 +0100

    fix fillRule property

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -5,7 +5,7 @@ const Menu = () => {
     return (
       <span className={style.menu}>
         <svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
-          <g fill="#151515" fill-rule="evenodd">
+          <g fill="#151515" fillRule="evenodd">
             <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
           </g>
         </svg>
