83f16e94fc77b0177268ddf02d39fbf6334deff3
divine <divineobi07@gmail.com>
2022-05-18 22:14:03 +0100

    refactor companList component

--- src/components/Modal/CompanyList.js
+++ src/components/Modal/CompanyList.js
@@ -1,27 +1,37 @@
+import React, { useState } from "react";
 import ArrowUp from "../../assets/icon-arrow-up.svg";
 import ArrowDown from "../../assets/icon-arrow-down.svg";
 
 import style from "./CompanyList.module.css";
 
 const CompanyList = () => {
+  const [active, setActive] = useState(false);
+  const toggleHandler = () => {
+    setActive(!active);
+  };
+
+  const controlArrow = active ? (
+    <span>
+      <img src={ArrowDown} alt="arrow down icon" />
+    </span>
+  ) : (
+    <span>
+      <img src={ArrowUp} alt="arrow up icon" />
+    </span>
+  );
   return (
     <li className={style.toggleList}>
-      <section className={style.inner}>
+      <section className={style.inner} onClick={toggleHandler}>
         <span>Company</span>
-        <span className={style.navigateArrow}>
-          <span>
-            <img src={ArrowUp} alt="arrow up icon" />
-          </span>
-          <span>
-            <img src={ArrowDown} alt="arrow down icon" />
-          </span>
-        </span>
+        <span className={style.navigateArrow}>{controlArrow}</span>
       </section>
-      <ul className={style.innerList}>
-        <li>History</li>
-        <li>Our Team</li>
-        <li>Blog</li>
-      </ul>
+      {active && (
+        <ul className={style.innerList}>
+          <li>History</li>
+          <li>Our Team</li>
+          <li>Blog</li>
+        </ul>
+      )}
     </li>
   );
 };
