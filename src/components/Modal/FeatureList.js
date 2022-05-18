326d8a57265e6f268bf0111565d58aeb923e99ba
divine <divineobi07@gmail.com>
2022-05-18 22:14:41 +0100

    refactor featurelist component

--- src/components/Modal/FeatureList.js
+++ src/components/Modal/FeatureList.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import ArrowUp from "../../assets/icon-arrow-up.svg";
 import ArrowDown from "../../assets/icon-arrow-down.svg";
 
@@ -9,45 +10,57 @@ import PlanImg from "../../assets/icon-planning.svg";
 import style from "./FeatureList.module.css";
 
 const FeatureList = () => {
+  const [activate, setActivate] = useState(false);
+
+  const toggleHandler = () => {
+    console.log("djdjdjd");
+    setActivate(!activate);
+  };
+
+  const changeArrow = activate ? (
+    <span>
+      <img src={ArrowDown} alt="arrow down icon" />
+    </span>
+  ) : (
+    <span>
+      <img src={ArrowUp} alt="arrow up icon" />
+    </span>
+  );
   return (
     <li className={style.toggleList}>
-      <section className={style.inner}>
+      <section className={style.inner} onClick={toggleHandler}>
         <span>Features</span>
-        <span className={style.navigateArrow}>
-          <span>
-            <img src={ArrowUp} alt="arrow up icon" />
-          </span>
-          <span>
-            <img src={ArrowDown} alt="arrow down icon" />
-          </span>
-        </span>
+        <span className={style.navigateArrow}>{changeArrow}</span>
       </section>
-      <ul className={style.innerList}>
-        <li>
-          <span>
-            <img src={TodoImg} alt="todo list icon" />
-          </span>
-          <span>TodoList</span>
-        </li>
-        <li>
-          <span>
-            <img src={CalendarImg} alt="todo list icon" />
-          </span>
-          <span>Calendar</span>
-        </li>
-        <li>
-          <span>
-            <img src={RemainderImg} alt="todo list icon" />
-          </span>
-          <span>Reminders</span>
-        </li>
-        <li>
-          <span>
-            <img src={PlanImg} alt="todo list icon" />
-          </span>
-          <span>Planning</span>
-        </li>
-      </ul>
+      
+      {activate && (
+        <ul className={style.innerList}>
+          <li>
+            <span>
+              <img src={TodoImg} alt="todo list icon" />
+            </span>
+            <span>TodoList</span>
+          </li>
+          <li>
+            <span>
+              <img src={CalendarImg} alt="todo list icon" />
+            </span>
+            <span>Calendar</span>
+          </li>
+          <li>
+            <span>
+              <img src={RemainderImg} alt="todo list icon" />
+            </span>
+            <span>Reminders</span>
+          </li>
+          <li>
+            <span>
+              <img src={PlanImg} alt="todo list icon" />
+            </span>
+            <span>Planning</span>
+          </li>
+        </ul>
+      )}
     </li>
   );
 };
