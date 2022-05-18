a5a40f53dbee652edc1c51c32d6940fe3efb901d
divine <divineobi07@gmail.com>
2022-05-18 22:11:42 +0100

    fix desktop modal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Logo from "./Logo";
 import Menu from "./Menu";
-import Modal from '../Modal/Modal'
+import FeatureList from "../Modal/FeatureList";
+import CompanyList from "../Modal/CompanyList";
+
 import style from './Header.module.css'
 
 const Header = () => {
@@ -9,10 +11,18 @@ const Header = () => {
     <header className={style.header}>
       <Logo />
       <Menu />
-      {/* <div className={style.desktopModal}>
-        <Modal />
-      </div> */}
-    
+      <nav className={style["desktop--navBar"]}>
+        <ul className={style["desktop--navList"]}>
+          <FeatureList />
+          <CompanyList />
+          <li>Careers</li>
+          <li>About</li>
+        </ul>
+        <div className={style.buttonContainer}>
+          <button className={style.loginBtn}>Login</button>
+          <button className={style.registerBtn}>Register</button>
+        </div>
+      </nav>
     </header>
   );
 };
