ffa887069de9eaeeaa0fe3a590ce062f23ae32cf
divine <divineobi07@gmail.com>
2022-05-18 12:16:56 +0100

    Create and add modal components to App.js.Create a react portal on the html file and linked on App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import style from "./App.module.css";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
+import Modal from "./components/Modal/Modal";
 
 const App = () => {
   return (
-    <div className={style["main-wrapper"]}>
-      <Header />
-      <MainContent />
-    </div>
+    <>
+      {ReactDOM.createPortal(<Modal />, document.querySelector('.modal'))}
+      <div className={style["main-wrapper"]}>
+        <Header />
+        <MainContent />
+      </div>
+    </>
   );
 };
 export default App;
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,12 @@
+import style from './Modal.module.css'
+
+const Modal = () => {
+    return <nav className={style.modal}>
+        <ul>
+            <li>Features</li>
+        </ul>
+    </nav>
+}
+
+
+export default Modal;
